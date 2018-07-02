ef0ab005e1a8dd8ec71615a3f9bbf74b5a07cc1d
Colton Berger <coltonberger@optonline.net>
2018-07-02 12:18:58 -0700

    wpi on submit login form

---
+++ public/javascripts/login.js
@@ -0,0 +1,16 @@
+//if the business partner box is checked submit route to http://localhost:3000/newevent
+//else submit route to http://localhost:3000/
+
+const submitButton = document.querySelector("#submit-form")
+const user = document.getElementById('user_checkbox');
+const business = document.getElementById('business_checkbox');
+
+document.addEventListener('DOMContentLoaded', function(){
+	submitButton.addEventListener('click', function(event){
+    if (user.checked) {
+      //route to index page
+    }
+    if (business.checked) {
+      //route to index_business page
+    }
+  });
