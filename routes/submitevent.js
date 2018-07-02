ca560ced6ef0f2e2e12d6baa6059d4e652163cdc
Priscilla Okawa <prisoka@gmail.com>
2018-07-02 00:12:13 -0700

    added route for submitting event, added route on app.js

--- routes/submitevent.js
+++ routes/submitevent.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET event's submission home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Priscilla' });
-
+  res.render('events_form');
 });
 
 
