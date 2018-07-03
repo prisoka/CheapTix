182d20a133a0ae00ecb6b671d0ba5140c21a5cd5
Colton Berger <coltonberger@optonline.net>
2018-07-03 14:12:20 -0700

    set up heroku db

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'CHANGE LATER WHEN DEPLOY'
+    connection: 'postgres://uvmuszzigdygou:ec9c1ee58b852c1b0cebc46e90a01c04da9197d980e8247d8ba6de13697b5bf2@ec2-54-235-249-33.compute-1.amazonaws.com:5432/db215118eee8g4'
   }
 };
