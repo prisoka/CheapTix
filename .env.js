e67dea7f996f6618557eb481cb83629be3040e2f
Colton Berger <coltonberger@optonline.net>
2018-07-06 11:31:33 -0700

    added client side validation

---
+++ .env.js
@@ -0,0 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+  require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`
+  });
+}
+
+module.exports = {
+  PGHOST: process.env.PGHOST,
+  PGUSER: process.env.PGUSER,
+  PGPASSWORD: process.env.PGPASSWORD,
+  PGDATABASE: process.env.PGDATABASE,
+};
