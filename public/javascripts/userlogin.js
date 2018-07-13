c84048b7238b6efe18e0296c4558d4bf2e3e823b
Priscilla Okawa <prisoka@gmail.com>
2018-07-12 23:19:04 -0700

    wip, login authentication

--- public/javascripts/userlogin.js
+++ public/javascripts/userlogin.js
@@ -1,19 +1,40 @@
-//if the business partner box is checked submit route to http://localhost:3000/newevent
-//else submit route to http://localhost:3000/
+window.onload = function() {
+  console.log('hellooooo!!!')
+}
 
-const loginBtn = document.querySelector("#login_submit-form");
-const createUserBtn = document.querySelector('create_submit-form');
+const apiUrl = 'http://localhost:3000';
+// const apiUrl = 'https://cheaptix.herokuapp.com'; //change to http://localhost:3000/ to use locally
+const loginUrl = apiUrl + '/login';
 
-const bCheckBox = document.getElementById('business_checkbox');
-const uCheckBox = document.getElementById('user_checkbox');
 
-document.addEventListener('DOMContentLoaded', function(){
-	loginBtn.addEventListener('click', function(event){
-    if (uCheckBox.checked) {
-      console.log("HEY user IS CHECKED!")
+function login(event){
+  console.log('HEY!')
+
+  event.preventDefault(); // prevent the webpage from refreshing
+
+  let email = document.getElementById('create_email_field').value;
+  let password = document.getElementById('create_password_field').value;
+
+  // fecth makes a network request
+  fetch(loginUrl, {
+    method: 'POST', // post HTTP method
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  })
+  .then(response => {
+    if (!response.ok){
+      throw new Error('request failed')
     }
-    if (bCheckBox.checked) {
-      //route to index_business page
-    }
-  });
-});
+    return response.json()
+  })
+  .then(() => {
+    console.log('CLEAR ME!')
+    document.getElementById("create_user_form").reset();
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
