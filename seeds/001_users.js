5c8c58cee183f53462f46d518fb65d202e6e04f4
Priscilla Okawa <prisoka@gmail.com>
2018-07-13 19:25:54 -0700

    wip, fixing views in navbar

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -8,8 +8,6 @@ exports.seed = function(knex, Promise) {
         {id: 1, user_type: 'business', email: 'pete@gmail.com', username: 'pete', password: '12345678'},
         {id: 2, user_type: 'customer', email: 'justin@gmail.com', username: 'justin', password: '56781234'},
         {id: 3, user_type: 'business', email: 'masha@gmail.com', username: 'masha', password: '1596321aa'},
-        {id: 4, user_type: 'customer', email: 'colton@gmail.com', username: 'colton', password: 'colton123'},
-        {id: 5, user_type: 'customer', email: 'pri@gmail.com', username: 'priscilla', password: 'priscilla'}
       ]);
     })
     .then(() => {
